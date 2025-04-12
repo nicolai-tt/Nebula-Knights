d8bde9f845c774b57f44a23d44a499defd1afd78
Nicolai <roccobrooker@gmail.com>
2025-04-12 13:50:44 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+// main.js - Starter JavaScript file
+
+// Variables
+const greeting = "Hello, world!";
+let counter = 0;
+
+// Function to increment the counter
+function incrementCounter() {
+    counter++;
+    console.log(`Counter is now: ${counter}`);
+}
+
+// Function to display greeting
+function showGreeting() {
+    console.log(greeting);
+}
+
+// Run functions
+showGreeting();
+incrementCounter();
