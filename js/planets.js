eef32669dcb7985b21ba4f4e90b74ce2fc837362
nicolai-tt <roccobrooker@gmail.com>
2025-04-20 11:33:17 -0400

    //

--- js/planets.js
+++ js/planets.js
@@ -4,14 +4,14 @@ let planetCard = document.getElementById("planet-link");
 let planetList = document.getElementById("planet-list");
 
 let planetImages = {
-  Mercury: "../images/mercury.jpg",
-  Venus: "../images/venus.jpg",
-  Earth: "https://upload.wikimedia.org/wikipedia/commons/9/97/The_Earth_seen_from_Apollo_17.jpg",
-  Mars: "../images/mars.jpg",
-  Jupiter: "../images/jupiter.jpg",
-  Saturn: "../images/saturn.jpg",
-  Uranus: "../images/uranus.jpg",
-  Neptune: "../images/neptune.jpg",
+  Mercury: "https://cdn.drop.media/nebulaknights/c3741e47-5a34-4cf8-9e42-8a79d2d024c3.png",
+  Venus: "https://cdn.drop.media/nebulaknights/fd4b7024-e248-4594-866b-22f03f2a6e13.jpeg",
+  Earth: "https://cdn.drop.media/nebulaknights/d7271f4f-3c7a-4bd6-bb26-3b8094bc53a9.jpeg",
+  Mars: "https://cdn.drop.media/nebulaknights/cf40c488-3ba7-45c2-8dfe-372cab941c80.jpeg",
+  Jupiter: "https://cdn.drop.media/guest/9bf73045-10fd-4490-9971-03a38f34dabb.jpeg",
+  Saturn: "https://cdn.drop.media/nebulaknights/c665e959-cd90-4beb-8c35-246a185fd554.jpeg",
+  Uranus: "https://cdn.drop.media/nebulaknights/2ed35673-302d-4fcb-b2cb-da4a30f2058d.png",
+  Neptune: "https://cdn.drop.media/nebulaknights/db3dd6aa-5b64-463a-b141-88de04e5c9af.jpeg",
 };
 
 async function fetchPlanetData() {
